1c997791862bc8b01b3c87bf20c3fb3f86e6a78b
Rahul Patil <rahulpatil2387@gmail.com>
2017-07-08 10:06:35 +0530

    configure shoulda matchers to use rspec and add testc case

--- spec/models/consumer_spec.rb
+++ spec/models/consumer_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Consumer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	# Test association
+  # ensure Consumer model has a m:m relationship with the Merchant model through transactions
+  it { should have_many(:transactions) }
+  it { should have_many(:merchants).dependent(:destroy) }
 end
