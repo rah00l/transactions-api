3b2121560612b7d6dff11325b2888348e086cbe4
Rahul Patil <rahulpatil2387@gmail.com>
2017-07-08 20:15:22 +0530

    Update indentation

--- spec/models/consumer_spec.rb
+++ spec/models/consumer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Consumer, type: :model do
-	# Test association
+  # Test association
   # ensure Consumer model has a m:m relationship with the Merchant model through transactions
   it { should have_many(:transactions) }
   it { should have_many(:merchants).dependent(:destroy) }
