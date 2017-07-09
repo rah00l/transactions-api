2f5beabefcfe6534c461895adff31d07aa543373
Rahul Patil <rahulpatil2387@gmail.com>
2017-07-09 13:30:29 +0530

    Add code for transactions POST request

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,9 @@
 class TransactionsController < ApplicationController
-  # GET /merchants
+  before_action :get_transactable, only: [:index]
+  # GET /consumers/:consumer_id/transactions
+  # GET /merchants/:merchant_id/transactions
   def index
+    json_response(@transactable.transactions)
   end
 
   # POST /merchants
@@ -9,5 +12,15 @@ class TransactionsController < ApplicationController
 
   # PUT /merchants/:id
   def update
+  end
+
+  private
+
+  def get_transactable
+    @transactable = if params[:consumer_id].present?
+      Consumer.find(params[:consumer_id])
+    else
+      Merchant.find(params[:merchant_id])
+    end
   end
 end
