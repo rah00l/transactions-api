49fe0166c725db323a1a55309c48f31615f943bd
Rahul Patil <rahulpatil2387@gmail.com>
2017-07-09 10:35:37 +0530

    Add code for merchants POST request

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -7,9 +7,18 @@ class MerchantsController < ApplicationController
 
   # POST /merchants
   def create
+    @merchant = Merchant.create!(merchant_params)
+    json_response(@merchant, :created)
   end
 
   # PUT /merchants/:id
   def update
+  end
+
+  private
+
+  def merchant_params
+    # whitelist params
+      params.permit(:name, :domain)
   end
 end
