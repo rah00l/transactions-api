22a8ea7857819601db648a8efa43eda102b0d3b6
Rahul Patil <rahulpatil2387@gmail.com>
2017-07-09 20:02:31 +0530

    Update seed file to have few records of consumer, merchant and transaction

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Few records of transactions, merchants and consumers
+
+## Consumers
+c1 = Consumer.create first_name: 'Nand', last_name: 'Kishor'
+c2 = Consumer.create first_name: 'Raj', last_name: 'Kumar'*
+c3 = Consumer.create first_name: 'Anvay', last_name: 'Lal'
+
+## Merchants
+m1 = Merchant.create name: 'Yatra', domain: 'https://www.yatra.com/'
+m2 = Merchant.create name: 'cinepolisindia', domain: 'http://www.cinepolisindia.com'
+m3 = Merchant.create name: 'chaipoint', domain: 'http://www.chaipoint.com'
+
+## Transactions
+Transaction.create consumer_id: c1.id, merchant_id: m1.id, sale_amount: (rand(11.2...76.9).round(2)), date: (rand*30).days.ago
+Transaction.create consumer_id: c1.id, merchant_id: m2.id, sale_amount: (rand(11.2...76.9).round(2)), date: (rand*30).days.ago
+
+Transaction.create consumer_id: c2.id, merchant_id: m1.id, sale_amount: (rand(11.2...76.9).round(2)), date: (rand*30).days.ago
+Transaction.create consumer_id: c2.id, merchant_id: m3.id, sale_amount: (rand(11.2...76.9).round(2)), date: (rand*30).days.ago
+
+Transaction.create consumer_id: c3.id, merchant_id: m3.id, sale_amount: (rand(11.2...76.9).round(2)), date: (rand*30).days.ago
